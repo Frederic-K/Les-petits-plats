caffd21e0638a7683b3f6586934e2f9f82b63d79
Frederic.K <frederic.kreuder@gmail.com>
2023-01-12 14:29:27 +0100

    CSS first tags list

--- scripts/tag-filter.js
+++ scripts/tag-filter.js
@@ -2,7 +2,7 @@
 /// DOM ///
 const ingredientFilterBtn = document.getElementsByClassName("tag__filter--ingredients")[0];
 const ingredientFilterTitle = document.getElementsByClassName("filter__ingredients")[0];
-const ingredientFilterInput = document.getElementsByClassName("filter__ingredient--input")[0];
+const ingredientFilterInput = document.getElementsByClassName("filter__ingredients--input")[0];
 const ingredientFilterIconChevronDown = document.getElementsByClassName("filter__ingredients--chevronDown")[0];
 const ingredientFilterIconChevronUp = document.getElementsByClassName("filter__ingredients--chevronUp")[0];
 const ingredientfilterList = document.getElementsByClassName("filter__ingredients--list")[0];
@@ -12,3 +12,16 @@ const ingredientfilterList = document.getElementsByClassName("filter__ingredient
 ingredientFilterBtn.addEventListener("click", () => dropdownIngredients());
 ingredientFilterBtn.addEventListener("keydonw", () => KeyboardDropdowIngredients());
 
+function dropdownIngredients() {
+    ingredientFilterTitle.classList.toggle("hidden")
+    ingredientFilterInput.classList.toggle("hidden")
+    ingredientFilterIconChevronDown.classList.toggle("hidden")
+    ingredientFilterIconChevronUp.classList.toggle("hidden")
+    ingredientfilterList.classList.toggle("hidden")
+    const isIngredientfilterListHidden = ingredientfilterList.classList.contains("hidden")
+    if (!isIngredientfilterListHidden) {
+        ingredientFilterBtn.setAttribute("aria-expanded", "true")
+    } else {
+        ingredientFilterBtn.setAttribute("aria-expanded", "false")
+    }
+}
