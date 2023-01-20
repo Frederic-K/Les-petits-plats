4110141acffb7b51d19aaf8a677ee0f688d6b4d6
Frederic.K <frederic.kreuder@gmail.com>
2023-01-20 09:57:29 +0100

    Rebuild class RecipeCard

--- js/factory/RecipeCard.js
+++ js/factory/RecipeCard.js
@@ -1,66 +1,72 @@
 
 class RecipeCard {
     constructor(data) {
-        this.wrapper = document.createElement("article")
-        this.name = data.name
-        this.time = data.time
-        // this.ingredient = recipes[0].ingredients[0].ingredient
-        // this.quantity = recipes[0].ingredients[0].quantity
-        // this.unit = recipes[0].ingredients[0].unit
-        this.description = data.description
+        console.log('dataFromRecipeCard', data);
+        this.name = data.name;
+        this.time = data.time;
+        this.ingredients = data.ingredients;
+        console.log('this.ingredients', this.ingredients);
+        this.description = data.description;
 
-        //this._createCard = (e) => this._createCard;
+        //this.createCard = (e) => this._createCard(e);
+
+        this._createCard();
     }
 
 
     _createCard() {
         this.recipeCardContent = `
-        <div class="recipe__card--imgContainer">
-        <img src="https://picsum.photos/200/300" alt="${this.name}" class="recipe__card--img" aria-label="${this.name}">
-    </div>
-    <div class="recipe__card--description">
-        <div class="recipe__card--header">
-            <h2 class="recipe__card--title">${this.name}</h2>
-            <div class="recipe__card--time">
-                <span class="recipe__card--timeIcon fa-regular fa-clock" aria-label="Icone temps de cuisson"></span>
-                <span class="recipe__card--cookingTime">${this.time} min</span>
+        <article class="recipe__card" aria-label="${this.name}">
+            <div class="recipe__card--imgContainer">
+            <img src="https://picsum.photos/200/300" alt="${this.name}" class="recipe__card--img" aria-label="${this.name}">
             </div>
-        </div>
-        <div class="recipe__card--explanation">
-            <p class="recipe__card--ingredients">
-                <span class="recipe__card--ingredientX">${this.ingredient} : ${this.quantity} ${this.unit}</span>
-            </p>
-            <p class="recipe__card--cookingDescription">${this.description}</p>
-        </div>
-    </div>
+            <div class="recipe__card--description">
+                <div class="recipe__card--header">
+                    <h2 class="recipe__card--title">${this.name}</h2>
+                    <div class="recipe__card--time">
+                        <span class="recipe__card--timeIcon fa-regular fa-clock" aria-label="Icone temps de cuisson"></span>
+                        <span class="recipe__card--cookingTime">${this.time} min</span>
+                    </div>
+                </div>
+                <div class="recipe__card--explanation">
+                    <p class="recipe__card--ingredients">
+                        <div class="recipe__card--ingredientX">
+                            ${getIngredientRecipeCardDOM(this.ingredients).join("")}
+                        </div>    
+                    </p>
+                    <p class="recipe__card--cookingDescription">${this.description}</p>
+                </div>
+            </div>
+        </article>    
         `
-        this.wrapper.innerHTML = this.recipeCardContent
-        this.wrapper.classList.add("recipe__card")
-        this.wrapper.setAttribute("tabindex", "0")
-        this.wrapper.setAttribute("aria-label", `${this.name}`)
-        return this.wrapper
     }
 }
 
-// async function displayRecipeCard() {
-//     const cardSection = document.getElementsByClassName("recipes__section")[0];
-//     const recipes = await getRecipesData()
-//     for (const recipe of recipes) {
-//         const newRecipeCard = new RecipeCard(recipe)
-//         const recipeCard = newRecipeCard._createCard()
-//         cardSection.appendChild(recipeCard)
-//     }
-// }
-// displayRecipeCard()
+function getIngredientRecipeCardDOM(data) {
+    let arrayIngredients = []  
+    for (let i = 0; i < data.length; i++) {
+        console.log('data.length', data.length);
+        let ingredients = data[i];
+        console.log('ingredients', ingredients);
+        let ingredientSection = `
+            <span>${ingredients.ingredient ? ingredients.ingredient : ""}</span>
+            <span>${ingredients.quantity ? ingredients.quantity : ""}  ${ingredients.unit ? ingredients.unit : ""}</span>
+            </br>
+        `
+        arrayIngredients.push(ingredientSection);
+    }    
+    return (arrayIngredients);
+};
+
 
 function displayRecipeCard(data) {
+    let recipeCard = ""
     const cardSection = document.getElementsByClassName("recipes__section")[0];
-    //const recipes = await getRecipesData()
-    for (const recipe of data) {
-        const newRecipeCard = new RecipeCard(recipe)
-        const recipeCard = newRecipeCard._createCard()
-        cardSection.appendChild(recipeCard)
+    for (const recipe of data) {        
+        getIngredientRecipeCardDOM(recipe)
+        recipeCard += new RecipeCard(recipe).recipeCardContent
     }
+    cardSection.innerHTML = recipeCard
 };
 
 async function displayAllRecipeCard() {
