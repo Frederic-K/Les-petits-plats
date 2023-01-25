6e198bd3bf355f85d9d0d8fdd81c67ce825afa92
Frederic.K <frederic.kreuder@gmail.com>
2023-01-25 10:07:10 +0100

    Switch tag function to tag class

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import Search from "./factory/Search.js";
+import TestTagFilter from "./factory/TagFilter.js";
 
 async function init() {
     const recipes = await getRecipesData()
     new Search(recipes);
+    new TestTagFilter(recipes);
 };
 
 init();
