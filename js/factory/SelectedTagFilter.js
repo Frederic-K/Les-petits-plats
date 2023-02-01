98b0f34f33b6cd5e019eb441ca4080f92af65892
Frederic.K <frederic.kreuder@gmail.com>
2023-02-01 10:08:20 +0100

    Creat new Class for selected tag filter

---
+++ js/factory/SelectedTagFilter.js
@@ -0,0 +1,39 @@
+/*export default*/ class TestSelectTag {
+    constructor(data) {
+        //console.log('data', data.target);
+        this.wrapper = document.createElement("div")
+        this.originFilter = data.target.parentElement
+        //console.log('color', this.originFilter);
+        this.filter = data.target.textContent; 
+        //this._createFilter();
+    };
+    _createFilter() {
+        this.filterTagContent = `
+            <li class="selectedFilter">${this.filter}</li>
+            <span class="fa-regular fa-circle-xmark circleCrossBtn"></span>
+        `
+        this.wrapper.innerHTML = this.filterTagContent
+        this.wrapper.classList.add("tag__parking--items")
+        this.wrapper.setAttribute("tabindex", "0")
+        this.wrapper.setAttribute("aria-label", `${this.filter}`)
+        if (this.originFilter.classList.contains("filter__ustensils--list")) {
+            this.wrapper.classList.add("bckground-red")
+        }
+        else if (this.originFilter.classList.contains("filter__appliances--list")) {
+            this.wrapper.classList.add("bckground-green")
+        }
+        else if (this.originFilter.classList.contains("filter__ingredients--list")) {
+            this.wrapper.classList.add("bckground-blue")
+        }
+        return this.wrapper
+    };
+
+};
+
+function displaySelectedFilter(event) {
+    const tagParking = document.getElementsByClassName("tag__parking")[0];
+    tagParking.classList.remove("hidden");
+    const newSelectedFilter = new TestSelectTag(event);
+    const selectedFilter = newSelectedFilter._createFilter();
+    tagParking.appendChild(selectedFilter);
+};
