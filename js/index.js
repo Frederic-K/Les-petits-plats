79cb38081d89ae339dabea1484a99b3a13c92dd0
Frederic.K <frederic.kreuder@gmail.com>
2023-02-21 15:52:29 +0100

    Clean code

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
 import Search from "./factory/Search.js";
-//import TagFilterCopy from "./factory/MenuTagFilter.js";
-//import RecipeCard from "./factory/RecipeCard.js";
-//import TagFilter from "./factory/TagFilter.js";
 
 async function init() {
     const recipes = await getRecipesData()
     new Search(recipes);
-    //new TagFilter(recipes);
-    //new TagFilterCopy(recipes);
-    //new RecipeCard(recipes);
 };
 
 init();
